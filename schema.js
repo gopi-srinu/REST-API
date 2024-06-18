ae90442b542f7448c87cb4eb6e82cbc21beb420e
Gopi Srinivas Pydipati <142145103+gopi-srinu@users.noreply.github.com>
2024-06-18 18:30:46 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const driver = new Schema({
+const task = new Schema({
     taskName: String,
     date: String,
     time: String,
     description: String
 }, {timestamps: true})
 
-const driverInfo = mongoose.model('driverInfo', driver);
-module.exports = driverInfo;
+const taskInfo = mongoose.model('taskInfo', task);
+module.exports = taskInfo;
